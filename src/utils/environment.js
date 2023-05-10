4997c7915a53a54ff9bdd4f38732894d9a493055
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-11 00:32:00 +0300

    export html

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,30 +1,30 @@
-const APP_NAME = "Teamlex";
-const DATA_API_KEY = ".data-api";
+// const APP_NAME = "Teamlex";
+// const DATA_API_KEY = ".data-api";
 
-const html = document.documentElement;
-const body = document.body;
-const isDebug = html.hasAttribute("data-debug");
+export const html = document.documentElement;
+export const body = document.body;
+// const isDebug = html.hasAttribute("data-debug");
 
-// ==========================================================================
-// GL TEST
-// ==========================================================================
-const canvas = document.createElement("canvas");
-let gl;
+// // ==========================================================================
+// // GL TEST
+// // ==========================================================================
+// const canvas = document.createElement("canvas");
+// let gl;
 
-try {
-  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-} catch (e) {
-  console.error(e);
-}
+// try {
+//   gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+// } catch (e) {
+//   console.error(e);
+// }
 
-const SUPPORT_WEBGL = gl ? true : false;
-if (!SUPPORT_WEBGL) {
-  html.classList.add("has-no-webgl-support");
-  alert(
-    "Your browser does not support WebGL. Unfortunately that means you will not get the full experience from our website and miss out on cool features. We recommend you to update your browser to a modern version!"
-  );
-}
+// const SUPPORT_WEBGL = gl ? true : false;
+// if (!SUPPORT_WEBGL) {
+//   html.classList.add("has-no-webgl-support");
+//   alert(
+//     "Your browser does not support WebGL. Unfortunately that means you will not get the full experience from our website and miss out on cool features. We recommend you to update your browser to a modern version!"
+//   );
+// }
 
-const models = [];
+// const models = [];
 
-export { APP_NAME, DATA_API_KEY, SUPPORT_WEBGL, html, body, isDebug, models };
+// export { APP_NAME, DATA_API_KEY, SUPPORT_WEBGL, html, body, isDebug, models };
