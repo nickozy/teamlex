fe4264325326a5a242ac982521eae0d4cadae559
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-11 03:01:12 +0300

    lazyload canonical

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,70 +1,69 @@
-/**
- * Get an image meta data
- *
- * @param {HTMLImageElement}    $img   - The image element.
- * @return {object}             The given image meta data
- */
+const LAZY_LOADED_IMAGES = [];
+const LOADED_IMAGES = [];
 
-const getImageMetadata = ($img) => ({
-  url: $img.src,
-  width: $img.naturalWidth,
-  height: $img.naturalHeight,
-  ratio: $img.naturalWidth / $img.naturalHeight,
-});
-
-/**
- * Load the given image.
- *
- * @param {string}  url         - The URI to lazy load into $el.
- * @param {object}  options     - An object of options
- * @return {void}
- */
-
-const loadImage = (url, options = {}) => {
+export function loadImage(url, options = {}) {
   return new Promise((resolve, reject) => {
-    const $img = new Image();
+    let loadedImage = LOADED_IMAGES.find((image) => image.url === url);
 
-    if (options.crossOrigin) {
-      $img.crossOrigin = options.crossOrigin;
-    }
-
-    const loadCallback = () => {
+    if (loadedImage) {
       resolve({
-        element: $img,
-        ...getImageMetadata($img),
+        ...loadedImage,
+        ...options,
       });
-    };
-
-    if ($img.decode) {
-      $img.src = url;
-      $img
-        .decode()
-        .then(loadCallback)
-        .catch((e) => {
-          reject(e);
-        });
     } else {
-      $img.onload = loadCallback;
-      $img.onerror = (e) => {
-        reject(e);
+      const $img = new Image();
+
+      if (options.crossOrigin) {
+        $img.crossOrigin = options.crossOrigin;
+      }
+
+      const loadCallback = () => {
+        const result = {
+          element: $img,
+          ...getImageMetadata($img),
+          ...options,
+        };
+        LOADED_IMAGES.push(result);
+        resolve(result);
       };
-      $img.src = url;
+
+      if ($img.decode) {
+        $img.src = url;
+        $img
+          .decode()
+          .then(loadCallback)
+          .catch((e) => {
+            reject(e);
+          });
+      } else {
+        $img.onload = loadCallback;
+        $img.onerror = (e) => {
+          reject(e);
+        };
+        $img.src = url;
+      }
     }
   });
-};
+}
+
+export function getImageMetadata($img) {
+  return {
+    url: $img.src,
+    width: $img.naturalWidth,
+    height: $img.naturalHeight,
+    ratio: $img.naturalWidth / $img.naturalHeight,
+  };
+}
 
 /**
  * Lazy load the given image.
  *
- * @param {HTMLImageElement}    $el      - The image element.
- * @param {?string}             url      - The URI to lazy load into $el.
+ * @param {HTMLImageElement} $el      - The image element.
+ * @param {?string}          url      - The URI to lazy load into $el.
  *     If falsey, the value of the `data-src` attribute on $el will be used as the URI.
- * @param {?function}           callback - A function to call when the image is loaded.
- * @return {void}
+ * @param {?function}        callback - A function to call when the image is loaded.
  */
-
-const LAZY_LOADED_IMAGES = [];
-const lazyLoadImage = async ($el, url, callback) => {
+export async function lazyLoadImage($el, url, callback) {
   let src = url ? url : $el.dataset.src;
 
   let loadedImage = LAZY_LOADED_IMAGES.find((image) => image.url === src);
@@ -90,17 +89,15 @@ const lazyLoadImage = async ($el, url, callback) => {
   }
 
   requestAnimationFrame(() => {
-    let lazyParent = $el.closest(".c-lazy");
+    let lazyParent = $el.closest(".c-image");
 
     if (lazyParent) {
-      lazyParent.classList.add("-lazy-loaded");
+      lazyParent.classList.add("-loaded");
       lazyParent.style.backgroundImage = "";
     }
 
-    $el.classList.add("-lazy-loaded");
+    $el.classList.add("-loaded");
 
     callback?.();
   });
-};
-
-export { getImageMetadata, loadImage, lazyLoadImage };
+}
