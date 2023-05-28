0d0234b13c27832aad125ab29ea6739b483714da
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-28 05:09:17 +0300

    update scroll

--- src/modules/Scroll.js
+++ src/modules/Scroll.js
@@ -111,6 +111,8 @@ export default class extends module {
 
     addScrollElements($newContainer) {
         this.locomotiveScrollInstance?.addScrollElements($newContainer)
+        window.Webflow.ready($newContainer);
+        window.Webflow.require('ix2').init($newContainer)
     }
 
     stop() {
