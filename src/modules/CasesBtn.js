0c9059b62382b06d11c441be1806e660fa023185
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-30 21:20:26 +0300

    add click filter

--- src/modules/CasesBtn.js
+++ src/modules/CasesBtn.js
@@ -14,15 +14,15 @@ export default class extends module {
 
         const wrapper = document.querySelector(".our-cases__nav-wrapper");
 
-        const text = btn.querySelector(".our-cases__show-text")
+        const text = btn.querySelector(".our-cases__show-text");
 
             btn.addEventListener("click", () => {
                 wrapper.classList.toggle("is-open");
-            btn.classList.toggle("is-open");
-            if (btn.classList.contains("is-open") === true) {
-            text.innerHTML = "Hide all tags";
+                btn.classList.toggle("is-open");
+                if (btn.classList.contains("is-open") === true) {
+                text.innerHTML = "Hide all tags";
             } else {
-            text.innerHTML = "Show all tags";
+            text.innerHTML = "Show all tags";   
             }
         }
         );
