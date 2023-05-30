beed148d85ed5e4dc0dcaa38a2c2585dfade92fe
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-30 20:36:38 +0300

    update scroll

--- src/modules/CasesBtn.js
+++ src/modules/CasesBtn.js
@@ -4,19 +4,18 @@ export default class extends module {
     constructor(m) {
         super(m);
     }
-init(){
-    const btn = this.el;
-    const wrapper = document.querySelector(".our-cases__nav-wrapper");
-    const text = document.querySelector(".our-cases__show-text")
-
-    btn.addEventListener("click", () => {
-        wrapper.classList.toggle("is-open");
-    btn.classList.toggle("is-open");
-    if (btn.classList.contains("is-open") === true) {
-    text.innerHTML = "Hide all tags";
-    } else {
-    text.innerHTML = "Show all tags";
-    }}
-    );
-}
+    init(){
+        const btn = this.el;
+        const wrapper = document.querySelector(".our-cases__nav-wrapper");
+        const text = document.querySelector(".our-cases__show-text")
+            btn.addEventListener("click", () => {
+                wrapper.classList.toggle("is-open");
+            btn.classList.toggle("is-open");
+            if (btn.classList.contains("is-open") === true) {
+            text.innerHTML = "Hide all tags";
+            } else {
+            text.innerHTML = "Show all tags";
+            }}
+        );
+    }
 }
