c304deee6c1ddd8ab9e2522950cef8e9d35c3d01
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-30 14:41:43 +0300

    filter add

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function init() {
   html.classList.remove("is-loading");
 }
 
-window.addEventListener("load", () => {
+document.addEventListener("load", () => {
   console.log("laoded");
   fsAttributes.cmsfilter.destroy();
   fsAttributes.cmsfilter.init();
