3209518a63ca56977d81b0f372ff1cb1bbb12028
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-30 20:35:10 +0300

    update scroll

--- src/modules.js
+++ src/modules.js
@@ -1,4 +1,5 @@
 export { default as Load } from "./modules/Load";
 export { default as Scroll } from "./modules/Scroll";
 export { default as ServicesSlider } from "./modules/ServicesSlider";
+export { default as CasesBtn } from "./modules/CasesBtn";
 // export { default as Filter } from "./modules/Filter";
