d1e54f8e20ec1bc4a56ab21ceb5a917e78884069
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-31 17:53:39 +0300

    video

--- vimeo.js
+++ vimeo.js
@@ -1,4 +1,4 @@
-var options = {
+const options = {
   id: 827324316,
   width: 1200,
   loop: false,
@@ -7,13 +7,46 @@ var options = {
 var player = new Vimeo.Player("video-container", options);
 
 player.muted = false;
+player.setVolume(1);
+const btn = document.querySelector(".video-button");
+const container = document.querySelector(".video-wrapper");
 
 // Open on play
-$("#video-trigger").click(function () {
-  player.setVolume(1);
-  player.play();
+btn.addEventListener("click", () => {
+  container.classList.toggle("play");
+
+  if (container.classList.contains("play") == true) {
+    player.play();
+  } else {
+    player.pause();
+  }
 });
 
-// player.on("play", () => {
-//   player.setVolume(1);
-// });
+//
+//
+////
+//////
+////////
+
+// const callback = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       console.log(entry.isIntersecting);
+
+//       observer.unobserve(entry.target);
+//     } else {
+//       console.log("Элемент пропал с поля видимости");
+//     }
+//   });
+// };
+
+const video = document.querySelector("[data-video]");
+const callback = (entry) => {
+  if (entry[0].isIntersecting === false) {
+    player.pause();
+    container.classList.remove("play");
+  }
+};
+const observer = new IntersectionObserver(callback);
+
+observer.observe(video);
