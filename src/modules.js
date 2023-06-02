4f356141b807ac8c024f94f8b4b3ce9abe33d6a2
nickozy <104327959+nickozy@users.noreply.github.com>
2023-06-02 16:19:39 +0300

    add CasesCarousel

--- src/modules.js
+++ src/modules.js
@@ -2,4 +2,5 @@ export { default as Load } from "./modules/Load";
 export { default as Scroll } from "./modules/Scroll";
 export { default as ServicesSlider } from "./modules/ServicesSlider";
 export { default as CasesBtn } from "./modules/CasesBtn";
+export { default as CasesCarousel } from "./modules/CasesCarousel";
 // export { default as Filter } from "./modules/Filter";
--- src/modules/CasesCarousel.js
+++ src/modules/CasesCarousel.js
@@ -1,1 +1,22 @@
 import Swiper from "swiper";
+import { module } from 'modujs';
+
+export default class extends module {
+    constructor(m) {
+        super(m);
+    }
+
+    init() {
+        const el = this.el
+        this.swiper = new Swiper(el.querySelector('.case-solution__cases-collection'), {
+        speed: 400,
+        slidesPerView: "auto",
+        wrapperClass: "case-solution__cases-list",
+        slideClass: "case-solution__cases-img",
+        navigation: {
+            nextEl:  el.querySelector(".arrow-nav__btn.is-next"),
+            prevEl:  el.querySelector(".arrow-nav__btn.is-prev"),
+          },
+        });
+    }
+}
