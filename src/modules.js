fe9588844c4e99d43469dfd7cbd5ac06e40d89df
nickozy <104327959+nickozy@users.noreply.github.com>
2023-06-13 01:31:24 +0300

    carousel companies

--- src/modules.js
+++ src/modules.js
@@ -4,4 +4,5 @@ export { default as ServicesSlider } from "./modules/ServicesSlider";
 export { default as CasesBtn } from "./modules/CasesBtn";
 export { default as CasesCarousel } from "./modules/CasesCarousel";
 export { default as CasesSpider } from "./modules/CasesSpider";
+export { default as CompaniesCarousel } from "./modules/CompaniesCarousel";
 // export { default as Filter } from "./modules/Filter";
