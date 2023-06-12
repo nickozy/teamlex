bb41d168f34598882b42f1a41b0ec20b64f100a0
nickozy <104327959+nickozy@users.noreply.github.com>
2023-06-13 01:27:38 +0300

    Create CompaniesCarousel.js

---
+++ src/modules/CompaniesCarousel.js
@@ -0,0 +1,17 @@
+import  Swiper from 'swiper';
+import { module } from 'modujs';
+
+export default class extends module {
+    constructor(m) {
+        super(m);
+    }
+
+    init() {
+        const el = this.el
+        this.swiper = new Swiper(el, {
+        speed: 400,
+        direction: 'vertical',
+        slidesPerView: "auto",
+        });
+    }
+}
