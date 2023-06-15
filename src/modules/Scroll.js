0811579ffde4970de18e434e8e3847351ee0b93c
nickozy <104327959+nickozy@users.noreply.github.com>
2023-06-15 13:20:11 +0300

    smooth false

--- src/modules/Scroll.js
+++ src/modules/Scroll.js
@@ -37,7 +37,7 @@ console.log("lenis init")
                 easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
                 direction: 'vertical', // vertical, horizontal
                 gestureDirection: 'vertical', // vertical, horizontal, both
-                smooth: true,
+                smooth: false,
                 mouseMultiplier: 1,
                 smoothTouch: false,
                 touchMultiplier: 2,
